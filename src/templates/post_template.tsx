e49f4df45233d318b01681bb0563a76efd726520
Kang Kujun <donato14@naver.com>
2023-01-30 21:40:40 +0900

    ⚡️ Print a markdown post

--- src/templates/post_template.tsx
+++ src/templates/post_template.tsx
@@ -1,13 +1,49 @@
 import React, { FunctionComponent } from 'react'
 import { graphql } from 'gatsby'
+import { PostPageItemType } from 'types/PostItem.types'
 import Template from 'components/Common/Template'
+import PostHead from 'components/Post/PostHead'
+import PostContent from 'components/Post/PostContent'
 
-type PostTemplateProps = {}
+type PostTemplateProps = {
+  data: {
+    allMarkdownRemark: {
+      edges: PostPageItemType[]
+    }
+  }
+}
 
-const PostTemplate: FunctionComponent<PostTemplateProps> = function (props) {
-  console.log(props)
+const PostTemplate: FunctionComponent<PostTemplateProps> = function ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) {
+  const {
+    node: {
+      html,
+      frontmatter: {
+        title,
+        summary,
+        date,
+        categories,
+        thumbnail: {
+          childImageSharp: { gatsbyImageData },
+        },
+      },
+    },
+  } = edges[0]
 
-  return <Template>Post Template</Template>
+  return (
+    <Template>
+      <PostHead
+        title={title}
+        date={date}
+        categories={categories}
+        thumbnail={gatsbyImageData}
+      />
+      <PostContent html={html} />
+    </Template>
+  )
 }
 
 export default PostTemplate
